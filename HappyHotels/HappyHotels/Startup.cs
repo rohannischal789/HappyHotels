0afc1367f931f88ea43b330a71f17e9531e2fa00
Rohan <rnis0005@student.monash.edu>
2019-09-13 19:56:41 +1000

    Project setup
    Project setup done with authentication

---
+++ HappyHotels/HappyHotels/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HappyHotels.Startup))]
+namespace HappyHotels
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
