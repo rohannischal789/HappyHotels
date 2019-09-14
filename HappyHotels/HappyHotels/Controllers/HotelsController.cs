d38b49b6c371397dcc2feea24398a4079558ed23
Rohan <rnis0005@student.monash.edu>
2019-09-14 22:10:29 +1000

    Index page done
    Index page done and added authorization for hotels

---
+++ HappyHotels/HappyHotels/Controllers/HotelsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HappyHotels.Models;
+using Microsoft.AspNet.Identity;
+
+namespace HappyHotels.Controllers
+{
+    public class HotelsController : Controller
+    {
+        private HappyHotelsEntities db = new HappyHotelsEntities();
+
+        // GET: Hotels
+        [Authorize]
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var hotels = db.Hotels.Where(s => s.user_id == userId).ToList();
+            return View(hotels);
+        }
+
+        // GET: Hotels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hotel hotel = db.Hotels.Find(id);
+            if (hotel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotel);
+        }
+
+        // GET: Hotels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Hotels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "hotel_id,name,description,address,city,state,country,postcode,user_id,check_in_time,check_out_time,lattitude,longitude,rating")] Hotel hotel)
+        {
+            hotel.user_id = User.Identity.GetUserId();
+            ModelState.Clear();
+            TryValidateModel(hotel);
+            if (ModelState.IsValid)
+            {
+                db.Hotels.Add(hotel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(hotel);
+        }
+
+        // GET: Hotels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hotel hotel = db.Hotels.Find(id);
+            if (hotel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotel);
+        }
+
+        // POST: Hotels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "hotel_id,name,description,address,city,state,country,postcode,user_id,check_in_time,check_out_time,lattitude,longitude,rating")] Hotel hotel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hotel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(hotel);
+        }
+
+        // GET: Hotels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hotel hotel = db.Hotels.Find(id);
+            if (hotel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotel);
+        }
+
+        // POST: Hotels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Hotel hotel = db.Hotels.Find(id);
+            db.Hotels.Remove(hotel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
